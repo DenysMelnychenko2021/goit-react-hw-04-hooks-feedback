ea5a24a1b296e4666e54f0e1d31c56ec4765332c
DenysMelnychenko2021 <83368161+DenysMelnychenko2021@users.noreply.github.com>
2022-01-10 21:54:56 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -12,9 +12,7 @@ const App = () => {
   const onLeaveFeedback = el =>
     setState(prevState => ({ ...prevState, [el]: prevState[el] + 1 }));
 
-  const countTotalFeedback = () => {
-    return good + neutral + bad;
-  };
+  const countTotalFeedback = () => good + neutral + bad;
 
   const countPositiveFeedbackPercentage = () =>
     (good / countTotalFeedback()) * 100;
@@ -46,57 +44,5 @@ const App = () => {
     </>
   );
 };
-
-/* class oldApp extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-
-  onLeaveFeedback = el =>
-    this.setState(prevState => {
-      return { [el]: prevState[el] + 1 };
-    });
-
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  };
-  countPositiveFeedbackPercentage = () =>
-    (this.state.good / this.countTotalFeedback()) * 100;
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-
-    return (
-      <>
-        <Container>
-          <Section title="Please leave feedback">
-            <FeedbackOptions
-              options={Object.keys(this.state)}
-              onLeaveFeedback={this.onLeaveFeedback}
-            />
-          </Section>
-
-          <Section title="Statistics">
-            {total === 0 ? (
-              <Notification message="No feedback given" />
-            ) : (
-              <Statistics
-                good={good}
-                neutral={neutral}
-                bad={bad}
-                total={this.countTotalFeedback}
-                positivePercentage={this.countPositiveFeedbackPercentage}
-              />
-            )}
-          </Section>
-        </Container>
-      </>
-    );
-  }
-} */
 
 export default App;
--- src/components/feedbackOptions/FeedbackOptions.jsx
+++ src/components/feedbackOptions/FeedbackOptions.jsx
@@ -1,16 +1,12 @@
 import PropTypes from 'prop-types';
 import s from './FeedbackOptions.module.css';
 
-const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-  console.log(options);
-  return options.map(el => {
-    return (
-      <button key={el} className={s.Button} onClick={() => onLeaveFeedback(el)}>
-        {el.toUpperCase()}
-      </button>
-    );
-  });
-};
+const FeedbackOptions = ({ options, onLeaveFeedback }) =>
+  options.map(el => (
+    <button key={el} className={s.Button} onClick={() => onLeaveFeedback(el)}>
+      {el.toUpperCase()}
+    </button>
+  ));
 
 FeedbackOptions.propTypes = {
   options: PropTypes.arrayOf(PropTypes.string.isRequired),
